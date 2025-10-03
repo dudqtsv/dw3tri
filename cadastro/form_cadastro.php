9be80897678a76ff12fcff68d3873f19ee5a8f2b
dudqtsv <dudacarrijo36@gmail.com>
2025-10-03 10:35:00 -0300

    mudanças

---
+++ cadastro/form_cadastro.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <div>
+        <h1>Criar nova conta</h1>
+        <form action="salvar_usuario.php" method="POST" enctype="multipart/form-data">
+            <p>Nome</p><input type="text" name="nome">
+            <p>Data de nascimento</p><input type="date" name="data_nascimento">
+            <p>E-mail</p><input type="email" name="email">
+            <p>Senha</p><input type="password" name="senha">
+            <p>Foto de perfil</p><input type="file" name="foto">
+            <p><input id="submit" type="submit"></p>
+        </form>
+    </div>
+    <a href="../index.php">Cancelar</a>
+</body>
+</html>
---
+++ cadastro/salvar_usuario.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+    require_once "../conexao.php"; 
+    
+    $nome = $_POST['nome'];
+    $data_nascimento = $_POST['data_nascimento'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $foto = $_FILES['foto'];
+    $_SESSION['nome_usuario'] = $nome;
+
+    if (empty($email) || empty($senha) || empty($nome) || empty($data_nascimento)) {
+        header("Location: erro.html");
+        exit();
+
+    $nome_arquivo = $_FILES['foto']['name'];
+    $caminho_temporario = $_FILES['foto']['tmp_name'];
+
+    //pegar a extensao do arquivo 
+    $extensao = pathinfo($nome_arquivo, PATHINFO_EXTENSION);
+
+    //gerar novo nome 
+    $novo_nome = uniqid() . "." . $extensao;
+
+    //lembre-se de criar a pasta e de ajustar as permissoes
+    $caminho_destino = "../fotos/" . $novo_nome;
+
+    move_uploaded_file($caminho_temporario, $caminho_destino);
+}
+    $sql = "INSERT INTO tb_usuario VALUES (null, ?, ?, ?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    // letra s -> varchar, date, datetime, char
+    // letra i -> int
+    // letra d -> float, decimal
+    mysqli_stmt_bind_param($comando, 'ssssss', $nome, $data_nascimento, $email, $senha, $novo_nome);
+
+    mysqli_stmt_execute($comando);
+
+    mysqli_stmt_close($comando);
+
+    header("Location: ../index.php");
+?>
